1d3dcdfeec3395f2add823ef0181b42f4eb9c902
benjo16 <benyitocch@gmail.com>
2018-05-17 21:27:09 -0400

    update_mi_app

---
+++ app/src/main/java/com/benjamin/study_jam_proyecto_final_v2018/MainActivity.java
@@ -0,0 +1,13 @@
+package com.benjamin.study_jam_proyecto_final_v2018;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/test/java/com/benjamin/study_jam_proyecto_final_v2018/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package com.benjamin.study_jam_proyecto_final_v2018;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
